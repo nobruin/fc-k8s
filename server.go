a6115fba59b1d91b9df0fae4d1988faf0546db57
nobruin <nobruin@gmail.com>
2024-05-14 16:05:40 -0300

    add config map envs on projects

--- server.go
+++ server.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/config", ConfigMap)
 	http.ListenAndServe(":80", nil)
 }
 
@@ -19,4 +21,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>This is an example of using environment variables</h1>"))
 
 	fmt.Fprintf(w, "Env1 %s and Env2 %s!!!", envVarEx1, envVarEx2)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("/config/config.txt")
+
+	if err != nil {
+		log.Fatal("Error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "Configs: %s ", string(data))
 }
